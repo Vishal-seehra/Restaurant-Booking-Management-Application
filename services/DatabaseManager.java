8ab677435437dbb4556a8294bb262470b1b8ebf4
Vishal seehra <118591224+Vishal-seehra@users.noreply.github.com>
2026-03-21 21:37:15 +0530

    Refactor DatabaseManager to improve connection handling
    Refactor getConnection method to throw SQLException instead of handling it internally.

--- services/DatabaseManager.java
+++ services/DatabaseManager.java
@@ -2,20 +2,16 @@ package services;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseManager {
-    conn = DriverManager.getConnection(
-    private static final String URL = "jdbc:mysql://restaurant-db.cl80c82c8jen.eu-north-1.rds.amazonaws.com:3306/restaurant_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
-;
+
+    private static final String URL = "jdbc:mysql://restaurant-db.cl80c82c8jen.eu-north-1.rds.amazonaws.com:3306/restaurant_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER = "admin";
     private static final String PASSWORD = "Seehra04";
-)
-    public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+
+    // Method to get DB connection
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
