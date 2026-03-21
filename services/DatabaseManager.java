5f37209758b0a8b09f75a99b95258a044b70ab2f
Vishal seehra <118591224+Vishal-seehra@users.noreply.github.com>
2026-03-21 20:59:39 +0530

    Modify database connection URL for SSL and timezone
    Updated database connection URL to include SSL and timezone parameters.

--- services/DatabaseManager.java
+++ services/DatabaseManager.java
@@ -4,11 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseManager {
-
-    private static final String URL = "jdbc:mysql://restaurant-db.cl80c82c8jen.eu-north-1.rds.amazonaws.com:3306/restaurant_db";
+    conn = DriverManager.getConnection(
+    private static final String URL = "jdbc:mysql://restaurant-db.cl80c82c8jen.eu-north-1.rds.amazonaws.com:3306/restaurant_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
+;
     private static final String USER = "admin";
     private static final String PASSWORD = "Seehra04";
-
+)
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
